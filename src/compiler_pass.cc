ce215fa3eac5ba81cadf2392337450f908bcebaa
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-10-28 15:09:26 -0400

    RAII wrappers for CXString and CXDiagnostic
    Throw exception in SinglePass constructor for compiler errors.

--- src/compiler_pass.cc
+++ src/compiler_pass.cc
@@ -3,7 +3,25 @@
 SinglePass::SinglePass(const Transformer & t_transformer __attribute__((unused)), const std::string & str)
     : CompilerPass(str),
       index_(clang_createIndex(0, 0)),
-      translation_unit_(clang_parseTranslationUnit(index_, temp_file_.name().c_str(), diagnostics.data(), static_cast<int>(diagnostics.size()), nullptr, 0, CXTranslationUnit_None)) {}
+      translation_unit_(clang_parseTranslationUnit(index_,
+                                                   temp_file_.name().c_str(),
+                                                   diagnostics.data(),
+                                                   static_cast<int>(diagnostics.size()),
+                                                   nullptr,
+                                                   0,
+                                                   CXTranslationUnit_None)) {
+  std::string all_diags = "";
+  for (unsigned i = 0, n = clang_getNumDiagnostics(translation_unit_); i != n; ++i) {
+    ClangDiagnostic diag(clang_getDiagnostic(translation_unit_, i));
+    ClangString diag_str(clang_formatDiagnostic(diag.get_raw(),
+                                                clang_defaultDiagnosticDisplayOptions()));
+    all_diags += diag_str.stl_str();
+  }
+
+  if (all_diags != "") {
+    throw std::logic_error("Compilation failed with diagnostics\n" + all_diags);
+  }
+}
 
 SinglePass::~SinglePass() {
   clang_disposeTranslationUnit(translation_unit_);
--- src/domino.cc
+++ src/domino.cc
@@ -1,11 +1,12 @@
-#include "clang-c/Index.h"
-
-#include "compiler_pass.h"
-
 #include <cstdio>
 #include <iostream>
 
 #include <string>
+
+#include "clang-c/Index.h"
+
+#include "util.h"
+#include "compiler_pass.h"
 
 std::string dummy(const CXTranslationUnit & t __attribute__((unused))) { return ""; }
 
@@ -16,7 +17,7 @@ int main(int argc, const char **argv) {
       return EXIT_FAILURE;
     } else {
       const std::string source_file(argv[1]);
-      SinglePass(dummy, source_file);
+      SinglePass(dummy, file_to_str(source_file));
     }
   } catch (const std::exception & e) {
     std::cerr << "Caught exception in main " << std::endl << e.what() << std::endl;
