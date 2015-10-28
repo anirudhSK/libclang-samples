8f1ce551ec8bce360a574e3a96b5d4a24eef93f7
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-10-28 10:52:16 -0400

    Throw an exception for compiler errors

--- src/dummy.cc
+++ src/dummy.cc
@@ -5,18 +5,36 @@
 #include <cstdio>
 #include <iostream>
 
-int main() {
-  CXIndex Index = clang_createIndex(0, 0);
-  const char* options[] = {"-Werror", "-Wall"};
-  CXTranslationUnit TU = clang_parseTranslationUnit(Index, "input.c", options, 2, nullptr, 0, CXTranslationUnit_None);
-  for (unsigned I = 0, N = clang_getNumDiagnostics(TU); I != N; ++I) {
-    CXDiagnostic Diag = clang_getDiagnostic(TU, I);
-    CXString String = clang_formatDiagnostic(Diag,
-    clang_defaultDiagnosticDisplayOptions());
-    fprintf(stderr, "%s\n", clang_getCString(String));
-    clang_disposeString(String);
-  }
+#include <string>
 
-  clang_disposeTranslationUnit(TU);
-  clang_disposeIndex(Index);
+int main(int argc, const char **argv) {
+  try {
+    if (argc != 2) {
+      std::cerr << "Usage: " << argv[0] << " <source_file>" << std::endl;
+      return EXIT_FAILURE;
+    } else {
+      CXIndex Index = clang_createIndex(0, 0);
+      const char* options[] = {"-Werror", "-Wall"};
+      CXTranslationUnit TU = clang_parseTranslationUnit(Index, argv[1], options, 2, nullptr, 0, CXTranslationUnit_None);
+      std::string all_diags = "";
+      for (unsigned I = 0, N = clang_getNumDiagnostics(TU); I != N; ++I) {
+        CXDiagnostic Diag = clang_getDiagnostic(TU, I);
+        CXString String = clang_formatDiagnostic(Diag,
+        clang_defaultDiagnosticDisplayOptions());
+        all_diags += std::string(clang_getCString(String)) + "\n";
+        clang_disposeString(String);
+      }
+
+      clang_disposeTranslationUnit(TU);
+      clang_disposeIndex(Index);
+      if (all_diags != "") {
+        throw std::logic_error("Compilation failed with diagnostics\n" + all_diags);
+      } else {
+        return EXIT_SUCCESS;
+      }
+    }
+  } catch (const std::exception & e) {
+    std::cerr << "Caught exception in main " << std::endl << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
 }
