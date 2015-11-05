dad5b8ee85d9cbe7d9045c4baf475e71e76c9754
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-11-05 08:29:42 -0500

    Print out CXCursorKind

--- src/ast_traversal.cc
+++ src/ast_traversal.cc
@@ -2,8 +2,11 @@
 
 #include <iostream>
 
+#include "src/clang_string.h"
+
 AstTraversal::AstTraversal(const CXTranslationUnit & translation_unit,
                            const CXCursor & cursor)
     : translation_unit_(translation_unit) {
   std::cout << PrettyPrinter(translation_unit_, cursor).output() << std::endl;
+  std::cout << ClangString(clang_getCursorKindSpelling(cursor.kind)).stl_str() << std::endl;
 }
